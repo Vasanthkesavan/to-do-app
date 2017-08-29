e5b462456489bc4c12d44a4b3a10cbadd0554325
vasanth <vk432@njit.edu>
2017-08-28 20:05:33 -0400

    init angular app

---
+++ to-do-app/src/app/task-c/task-c.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-task-c',
+  templateUrl: './task-c.component.html',
+  styleUrls: ['./task-c.component.css']
+})
+export class TaskCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
